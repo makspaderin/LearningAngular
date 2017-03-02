fac6c107c6ab6f3aa8a10317de036d83de757128
makspaderin <mvpaderin@gmail.com>
2017-03-02 22:02:05 +0200

    Added scripts

---
+++ app.js
@@ -0,0 +1,1 @@
+angular.module("ngLearn", ['ui.bootstrap']);
---
+++ scripts/learnController.js
@@ -0,0 +1,5 @@
+angular
+      .module('ngLearn')
+      .controller('learnController', function($scope) {
+          $scope.hello = 'Maksim';
+      });
