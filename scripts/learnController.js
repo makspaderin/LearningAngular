8f088fc039c328c8bc06528f8e11fa25b28e7181
makspaderin <mvpaderin@gmail.com>
2017-03-03 21:24:19 +0200

    Third lesson passed

--- scripts/learnController.js
+++ scripts/learnController.js
@@ -1,5 +1,21 @@
 angular
       .module('ngLearn')
       .controller('learnController', function($scope) {
-          $scope.hello = 'Maksim';
+          $scope.data = [
+            {
+              "name": "Maksim",
+              "surname": "Paderin",
+              "pupsiness": 600
+            },
+            {
+              "name": "Katya",
+              "surname": "Ladeichshikova",
+              "pupsiness": 999
+            },
+            {
+              "name": "Johhny",
+              "surname": "Cat",
+              "pupsiness": 400
+            },
+          ];
       });
